4bee03f2bb6fdd473e93bdb30dc5c93562d4924c
juliesantiago <santiago.sjulie@gmail.com>
2023-05-25 16:52:15 -0300

    feat: :sparkles: Creates AuthUserProvider

---
+++ src/context/AuthUserProvider.js
@@ -0,0 +1,14 @@
+import React, {createContext, useState} from 'react';
+
+export const AuthUserContext = createContext({});
+
+export const AuthUserProvider = ({children}) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AuthUserContext.Provider value={(user, setUser)}>
+      {children}
+    </AuthUserContext.Provider>
+    //todos que estiverem abaixo desse provider, receberão esses valores
+  );
+};
--- src/screens/Flowers.js
+++ src/screens/Flowers.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import { View, FlatList, SafeAreaView } from 'react-native';
+import {View, Text} from 'react-native';
 
-// import { Container } from './styles';
-const dadinhos = [
-    {nome: 'rosa',}
-]
 const Flowers = () => {
-    return (
-        <SafeAreaView>
-          <View style={style.pageHome}>
-            <FlatList //cada item dentro da flatlist renderiza o componente que for passado na função render
-              data={dadinhos}
-              //renderItem={renderItem}
-              keyExtractor={item => item.id}
-            />
-            {/* {loading && <LoadingIndicator />} */}
-            {/* usando javascript para criar a lógica, passando um state (loading) */}
-            {/* se state é verdadeiro, LoadingIndicator vai aparecer */}
-          </View>
-          <View style={style.bottom}>
-            <LogoutButton style={style.button} />
-          </View>
-        </SafeAreaView>
-      );
-}
+  return (
+    <View>
+      <Text>Página de todas as flores disponíveis</Text>
+    </View>
+  );
+};
 
-export Flowers ;
+export default Flowers;
